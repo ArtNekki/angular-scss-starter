0e3e446735cad3e38e15668c5c95b028bdfda391
a.mayatskiy <work.mayatskiy@gmail.com>
2024-02-27 18:49:51 +1000

    refactor: update ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import {AuthInterceptor} from './shared/services/auth-interceptor.service'
       : StoreDevtoolsModule.instrument({
           maxAge: 25,
           logOnly: environment.production,
+          connectInZone: true,
         }),
     StoreRouterConnectingModule.forRoot(),
   ],
